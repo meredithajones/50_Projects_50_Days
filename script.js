2c1181e02d27e2dc60d7c831abcc83cbca6901ac
Meredith Jones <mj19003@gmail.com>
2021-05-26 13:04:42 -0400

    Added function to add or remove status of active from bar

--- script.js
+++ script.js
@@ -8,4 +8,31 @@ let currentActive = 1
 // Adding event listner to next button 
 next.addEventListener('click', () => {
     currentActive++
+// making sure that it will not go beyond the number of circles.
+    if(currentActive > circles.length) {
+        currentActive = circles.length
+    }
+        update()
+
 })
+
+prev.addEventListener('click', () => {
+    currentActive--
+// making sure that it will not go beyond the number of circles.
+    if(currentActive < 1) {
+        currentActive = 1
+    }
+
+        update()
+})
+
+function update() {
+    circles.forEach((circle, idx) => {
+        if(idx < currentActive) {
+            circle.classList.add('active')
+        } else {
+            circle.classList.remove('active')
+        }
+    }
+    )
+}
