e02899f92416810714459fdee0f5ee791d1b42ef
Meredith Jones <mj19003@gmail.com>
2021-05-26 12:28:04 -0400

    Building out Javascript, defined elements and added button event listener

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-const progress = document.getElementById('progress')
+const progress = document.getElementById('progress');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+const circles = document.querySelectorAll('.circle');
+
+let currentActive = 1
+
+// Adding event listner to next button 
+next.addEventListener('click', () => {
+    currentActive++
+})
