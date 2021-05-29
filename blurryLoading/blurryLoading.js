b23a2f42a3ec8b9357760035683bce3d270c4021
Meredith Jones <mj19003@gmail.com>
2021-05-29 11:58:05 -0400

    Added js to get percentage count visually working

--- blurryLoading/blurryLoading.js
+++ blurryLoading/blurryLoading.js
@@ -3,8 +3,14 @@ const bg = document.querySelector('.bg');
 
 let load = 0;
 
+let int = setInterval(blurring, 30);
+
 function blurring() {
     load ++
 
-    console.log(load)
+    if(load > 99) {
+        clearInterval(int)
+    }
+
+    loadText.innerText = `${load}%`;
 }
