f9fa96e30874d7b20082804bf15fd1d4b6f45d0e
Meredith Jones <mj19003@gmail.com>
2021-05-31 11:06:36 -0400

    Added js to expand on hover for both left and right sides + retract on mouseleave

--- splitLandingPage/splitLandingPage.js
+++ splitLandingPage/splitLandingPage.js
@@ -3,4 +3,14 @@ const right = document.querySelector('.right');
 const container = document.querySelector('.container');
 
 left.addEventListener('mouseenter', () => 
-container.classList.add('hover.left'));
+container.classList.add('hover-left'));
+
+left.addEventListener('mouseleave', () => 
+container.classList.remove('hover-left'));
+
+
+right.addEventListener('mouseenter', () => 
+container.classList.add('hover-right'));
+
+right.addEventListener('mouseleave', () => 
+container.classList.remove('hover-right'));
