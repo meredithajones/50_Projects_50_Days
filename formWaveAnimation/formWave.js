175ebb9dab9134a290e3bdaa40c7dc651ec288d5
Meredith Jones <mj19003@gmail.com>
2021-05-31 17:07:51 -0400

    Added notation to functions

--- formWaveAnimation/formWave.js
+++ formWaveAnimation/formWave.js
@@ -2,7 +2,10 @@ const labels = document.querySelectorAll(".form-control label")
 
 labels.forEach(label => {
     label.innerHTML = label.innerText
+    // splitting the inner html into an array, so each letter has its own item in array
     .split('')
-    // Using .map to turn our current array into an array of letters
+    // Using .map to turn our current array into an array of letters with span around it
     .map((letter, idx) => <span>${letter}</span>)
+    // using .join to turn array back into a string
+    .join('')
 })
