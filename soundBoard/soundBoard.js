176e408b70b5efa11ebe78a7e919b89d95d15dac
Meredith Jones <mj19003@gmail.com>
2021-06-01 15:13:36 -0400

    Added js to dynamically create buttons with sound names

--- soundBoard/soundBoard.js
+++ soundBoard/soundBoard.js
@@ -1,5 +1,11 @@
-const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+const sounds = ['applause', 'boo', 'gasp', 'tada', 
+'victory', 'wrong']
 
-sounds.forEach(sound =>{
-    const btn =document.createElement('button')
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    document.getElementById('buttons').appendChild(btn)
 })
