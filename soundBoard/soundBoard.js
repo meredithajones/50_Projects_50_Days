a3548a85417b8a6d6a982986358dfdc50fcf5684
Meredith Jones <mj19003@gmail.com>
2021-06-01 15:28:07 -0400

    Added functions to get sounds playing

--- soundBoard/soundBoard.js
+++ soundBoard/soundBoard.js
@@ -7,5 +7,9 @@ sounds.forEach(sound => {
 
     btn.innerText = sound
 
+    btn.addEventListener('click', () => {
+        document.getElementById(sound).play()
+    })
+
     document.getElementById('buttons').appendChild(btn)
 })
