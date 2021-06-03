20358c1c81e9161d255d55ffe38bc9a6f33abb92
Meredith Jones <mj19003@gmail.com>
2021-06-03 15:06:42 -0400

    converted fetch into async await variable

--- dadJokes/dadJokes.js
+++ dadJokes/dadJokes.js
@@ -5,19 +5,32 @@ jokeBtn.addEventListener('click', generateJoke)
 
 generateJoke()
 
-function generateJoke() {
-    const config = {
-    //Adding headers to accept values as JSON rather than HTML
-    headers: {
-        Accept: 'application/json',
+async function generateJoke() {
+        const config = {
+        //Adding headers to accept values as JSON rather than HTML
+        headers: {
+            Accept: 'application/json',
+            }
         }
-    }
+    
+        //creating variable response to equal "fetch"
+        const response = await fetch('https://icanhazdadjoke.com', config)
+       
+  
 
-    //using fetch to access the joke API
-    fetch('https://icanhazdadjoke.com', config)
-    .then((res) => res.json())
-    .then((data) => {
-    // console.log(data.joke))
-    jokeEl.innerHTML = data.joke
-})
-}
+// function generateJoke() {
+//     const config = {
+//     //Adding headers to accept values as JSON rather than HTML
+//     headers: {
+//         Accept: 'application/json',
+//         }
+//     }
+
+//     //using fetch to access the joke API
+//     fetch('https://icanhazdadjoke.com', config)
+//     .then((res) => res.json())
+//     .then((data) => {
+//     // console.log(data.joke))
+//     jokeEl.innerHTML = data.joke
+// })
+// }
