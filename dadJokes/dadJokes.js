611b8bd81ab4f7999db5968d57fc6be71b18700c
Meredith Jones <mj19003@gmail.com>
2021-06-03 13:54:38 -0400

    Stored accept json in config variable

--- dadJokes/dadJokes.js
+++ dadJokes/dadJokes.js
@@ -1,2 +1,17 @@
 const jokeEl = document.getElementById('joke');
 const jokeBtn =document.getElementById('jokeBtn');
+
+generateJoke()
+
+function generateJoke() {
+    const config = {
+    //Adding headers to accept values as JSON rather than HTML
+    headers: {
+        'Accept': 'application/json'
+        }
+    }
+
+    //using fetch to access the joke API
+    fetch('https://icanhazdadjoke.com', config)
+    
+}
