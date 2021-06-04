50710fd9314b3b5839252b82b63afa5f47c010ee
Meredith Jones <mj19003@gmail.com>
2021-06-04 16:43:20 -0400

    Set up template literal to pull in data to html

--- eventKeycodes/eventKey.js
+++ eventKeycodes/eventKey.js
@@ -3,12 +3,12 @@ const insert = document.getElementById('insert')
 window.addEventListener('keydown', (e) => {
     insert.innerHTML = `
     <div class="key">
-    a
+    ${e.key === '' ? 'Space' : e.key}
     <small>event.key</small>
 </div>
 
 <div class="key">
- 65 
+    ${e.keyCode}
  <small>event.keycode</small>
 </div>
 
