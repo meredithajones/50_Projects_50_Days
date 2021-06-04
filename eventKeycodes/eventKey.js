11f982bca0db4725ec9ca0412d69c09059499dc1
Meredith Jones <mj19003@gmail.com>
2021-06-04 16:37:16 -0400

    Adding event listner

---
+++ eventKeycodes/eventKey.js
@@ -0,0 +1,20 @@
+const insert = document.getElementById('insert')
+
+window.addEventListener('keydown', (e) => {
+    insert.innerHTML = `
+    <div class="key">
+    a
+    <small>event.key</small>
+</div>
+
+<div class="key">
+ 65 
+ <small>event.keycode</small>
+</div>
+
+<div class="key">
+    KeyA 
+    <small>event.code</small>
+    </div>
+    `
+})
