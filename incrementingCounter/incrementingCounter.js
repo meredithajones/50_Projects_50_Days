c87c557c0b1aa06e1dab29d66aacf24727ee8ebc
Meredith Jones <mj19003@gmail.com>
2021-06-08 14:26:35 -0400

    Added if else statement to update the counter for as long as it is not at target number and set timeout

--- incrementingCounter/incrementingCounter.js
+++ incrementingCounter/incrementingCounter.js
@@ -5,13 +5,15 @@ counters.forEach(counter => {
 
      const updateCounter = () => {
          const target = +counter.getAttribute('data-target')
-         const c = counter.innerText
+         const c = +counter.innerText
 
          const increment = target / 200
 
          if(c < target) {
              counter.innerText = `${Math.ceil(c + increment)}`
-            setTimeoout(updateCounter, 
+            setTimeout(updateCounter, 1)
+         } else {
+             counter.innerText = target 
          }
      }   
 
