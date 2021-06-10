3433b91e36b17dacf1103277c21debdbeb33a7b6
Meredith Jones <mj19003@gmail.com>
2021-06-10 16:23:27 -0400

    Defined variables from the DOM and added forEach loop for cups

---
+++ drinkWater/drinkWater.js
@@ -0,0 +1,8 @@
+const smallCups = document.querySelectorAll('.cup-small')
+const liters = document.getElementById('.liters')
+const percentage = document.getElementById('.percentage')
+const remaining = document.getElementById('.remaining')
+
+smallCups.forEach((cup, idx) => {
+    console.log(idx)
+})
