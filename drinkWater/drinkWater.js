24ee12a57f71c64b990a132921fc99277ce32f33
Meredith Jones <mj19003@gmail.com>
2021-06-11 10:04:59 -0400

    Finished project with bugs remaining to be fixed

--- drinkWater/drinkWater.js
+++ drinkWater/drinkWater.js
@@ -1,7 +1,10 @@
 const smallCups = document.querySelectorAll('.cup-small')
 const liters = document.getElementById('.liters')
-const percentage = document.getElementById('.percentage')
-const remaining = document.getElementById('.remaining')
+const percentage = document.getElementById('percentage')
+const waterLeft = document.getElementById('.waterLeft')
+
+console.log(waterLeft);
+
 
 updateBigCup()
 
@@ -9,9 +12,9 @@ smallCups.forEach((cup, idx) => {
     cup.addEventListener('click', () => highlightCups(idx))
 })
 
-function highlightCups(idx){
-    if(smallCups[idx].classList.contains('full') && !smallCups
-    [idx].nextElementSibling.classList.contains('full')) {
+function highlightCups(idx) {
+    if (idx===7 && smallCups[idx].classList.contains("full")) idx--;
+    else if(smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')) {
         idx--
     }
 
@@ -22,14 +25,29 @@ function highlightCups(idx){
             cup.classList.remove('full')
         }
     })
+
     updateBigCup()
 }
+
 
 function updateBigCup() {
     const fullCups = document.querySelectorAll('.cup-small.full').length
     const totalCups = smallCups.length
-    
+
     if(fullCups === 0) {
         percentage.style.visibility = 'hidden'
+        percentage.style.height = 0
+    } else {
+        percentage.style.visibility = 'visible'
+        percentage.style.height = `${fullCups / totalCups * 330}px`
+        percentage.innerText = `${fullCups / totalCups * 100}%`
     }
-}   
+
+    if(fullCups === totalCups) {
+        waterLeft.style.visibility = 'hidden'
+        waterLeft.style.height = 0
+    // } else {
+    //     waterLeft.style.visibility = 'visible'
+    //     liters.innerText = `${2 - (250 * fullCups / 1000)}L`
+    }
+}
