ba2b9ad1173de0940929ba0bca72e1da9b15aca6
Meredith Jones <mj19003@gmail.com>
2021-06-11 17:27:41 -0400

    Added getMovie function to test hitting the API with res & data

--- movieApp/movieApp.js
+++ movieApp/movieApp.js
@@ -1,3 +1,13 @@
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=b100135861111ccc00fb1cc54bf3add1&page=1'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?&api_key=b100135861111ccc00fb1cc54bf3&query="'
+
+
+//get Initial Movies
+getMovies(API_URL)
+async function getMovies(url) {
+    const res = await fetch(url)
+    const data = await res.json()
+
+    console.log(data.results)
+}
