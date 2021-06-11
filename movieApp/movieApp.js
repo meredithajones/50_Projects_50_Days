1720c1af1b782bf8b1696a8e72bbe53a83177a86
Meredith Jones <mj19003@gmail.com>
2021-06-11 17:24:01 -0400

    Defined api url, img path & search url in javascript

---
+++ movieApp/movieApp.js
@@ -0,0 +1,3 @@
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=b100135861111ccc00fb1cc54bf3add1&page=1'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?&api_key=b100135861111ccc00fb1cc54bf3&query="'
