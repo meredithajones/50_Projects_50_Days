3177305e531b9e0f413c6c7ce61d2aa2680d5345
Meredith Jones <mj19003@gmail.com>
2021-06-14 17:44:50 -0400

    Defined variables and set up arrays for days & months

---
+++ themeClock/themeClock.js
@@ -0,0 +1,11 @@
+const hourEl = document.querySelector('.hour')
+const minutesEl = document.querySelector('.minute')
+const secondEl = document.querySelector('.second')
+const timeEl = document.querySelector('.time')
+const dateEl = document.querySelector('.date')
+const toggelEl = document.querySelector('.toggle')
+
+//create arrays for days and for months
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
