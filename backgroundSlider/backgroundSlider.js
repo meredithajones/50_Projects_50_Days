798310f0a1269003d79b896b62ea623c2578f6af
Meredith Jones <mj19003@gmail.com>
2021-06-14 14:53:34 -0400

    Added event listners to right and left buttons to move through images

--- backgroundSlider/backgroundSlider.js
+++ backgroundSlider/backgroundSlider.js
@@ -1,9 +1,31 @@
 const body = document.body 
 const slides = document.querySelectorAll('.slide')
-const leftBtn = document.getElementById('.left')
-const rightBtn = document.getElementById('.right')
+const leftBtn = document.getElementById('left')
+const rightBtn = document.getElementById('right')
 
 let activeSlide = 0;
+
+rightBtn.addEventListener('click', () => {
+    activeSlide++
+
+    if(activeSlide > slides.length -1) {
+        activeSlide = 0
+    }
+
+    setBackgroundToBody()
+    setActiveSlide()
+})
+
+leftBtn.addEventListener('click', () => {
+    activeSlide++
+
+    if(activeSlide > slides.length -1) {
+        activeSlide = 0
+    }
+
+    setBackgroundToBody()
+    setActiveSlide()
+})
 
 setBackgroundToBody()
 
