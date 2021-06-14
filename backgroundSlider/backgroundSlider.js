00c7dd9ea6fae11a499e46e79e2a00f297c91ab7
Meredith Jones <mj19003@gmail.com>
2021-06-14 14:43:55 -0400

    Added setActive Slide Function

--- backgroundSlider/backgroundSlider.js
+++ backgroundSlider/backgroundSlider.js
@@ -3,9 +3,19 @@ const slides = document.querySelectorAll('.slide')
 const leftBtn = document.getElementById('.left')
 const rightBtn = document.getElementById('.right')
 
-left activeSlide = 0;
+let activeSlide = 0;
+
+setBackgroundToBody()
 
 function setBackgroundToBody() {
     body.style.backgroundImage = slides[activeSlide].style.
     backgroundImage
+}
+
+function setActiveSlide() {
+    slides.forEach(slide => 
+        slide.classList.remove('active'))
+
+        slides[activeSlide].classList.add('active')
+    
 }
