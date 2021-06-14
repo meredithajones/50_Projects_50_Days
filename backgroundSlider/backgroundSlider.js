335837f640aedd5246c75916085cab09cf7422c0
Meredith Jones <mj19003@gmail.com>
2021-06-14 14:56:50 -0400

    Finished finctionality for left arrow button. Project complete

--- backgroundSlider/backgroundSlider.js
+++ backgroundSlider/backgroundSlider.js
@@ -17,10 +17,10 @@ rightBtn.addEventListener('click', () => {
 })
 
 leftBtn.addEventListener('click', () => {
-    activeSlide++
+    activeSlide--
 
-    if(activeSlide > slides.length -1) {
-        activeSlide = 0
+    if(activeSlide < 0) {
+        activeSlide = slides.length -1
     }
 
     setBackgroundToBody()
