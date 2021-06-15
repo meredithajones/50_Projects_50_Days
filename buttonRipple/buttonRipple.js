b3e271d2280744f3ac4fdeb34472143e95e8b2e1
Meredith Jones <mj19003@gmail.com>
2021-06-15 14:26:57 -0400

    Set up variables for calculating exact locations of clicks within the button

--- buttonRipple/buttonRipple.js
+++ buttonRipple/buttonRipple.js
@@ -2,7 +2,17 @@ const buttons = document.querySelectorAll('.ripple')
 
 buttons.forEach(button => {
     button.addEventListener('click', (e) => {
+        // client X & client Y will get location on axis of click
         const x = e.clientX
         const y = e.clientY
+
+        const buttonTop = e.target.offsetTop
+        const buttonLeft = e.target.offsetLeft
+
+        const xInside = x - buttonLeft
+        const yInside = y - buttonTop
+
+        console.log(xInside, yInside);
+
     })
 })
