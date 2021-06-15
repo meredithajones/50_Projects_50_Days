4e0cc7b385cee361912b92fa2f41aa5a9d75be59
Meredith Jones <mj19003@gmail.com>
2021-06-15 14:33:32 -0400

    Circle animation funcitoning

--- buttonRipple/buttonRipple.js
+++ buttonRipple/buttonRipple.js
@@ -1,7 +1,7 @@
 const buttons = document.querySelectorAll('.ripple')
 
 buttons.forEach(button => {
-    button.addEventListener('click', (e) => {
+    button.addEventListener('click', function (e) {
         // client X & client Y will get location on axis of click
         const x = e.clientX
         const y = e.clientY
@@ -12,7 +12,13 @@ buttons.forEach(button => {
         const xInside = x - buttonLeft
         const yInside = y - buttonTop
 
-        console.log(xInside, yInside);
+        const circle = document.createElement('span')
+        circle.classList.add('circle')
+        circle.style.top = yInside + 'px'
+        circle.style.left = xInside + 'px'
+
+        // Because we are using "this" we cannot use an arrow function
+        this.appendChild(circle);
 
     })
 })
