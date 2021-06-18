4df8d7481aa4c314fcd1a8a7daca5b5071b4eb81
Meredith Jones <mj19003@gmail.com>
2021-06-18 17:09:11 -0400

    Added drawLine function

--- drawingApp/drawingApp.js
+++ drawingApp/drawingApp.js
@@ -12,4 +12,14 @@ function drawCircle (x, y) {
     ctx.fill()
 }
 
+function drawLine(x1, y1, x2, y2) {
+    ctx.beginPath()
+    ctx.moveTo(x1, y1)
+    ctx.lineTo(x2, y2)
+    ctx.strokeStyle = color
+    ctx.lineWidth = size 
+    ctx.stroke()
+}
+
 drawCircle(200, 400)
+drawLine(300, 300, 300, 500)
