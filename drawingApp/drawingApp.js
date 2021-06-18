0a5769382ea61d788e79019611994b95bc8f979a
Meredith Jones <mj19003@gmail.com>
2021-06-18 17:16:47 -0400

    Added event listener to canvas for mouse release

--- drawingApp/drawingApp.js
+++ drawingApp/drawingApp.js
@@ -12,8 +12,15 @@ canvas.addEventListener('mousedown', (e) => {
 
     x = e.offsetX
     y = e.offsetY
+})
 
-    console.log(isPressed, x, y)
+
+canvas.addEventListener('mouseup', (e) => {
+    isPressed = false 
+
+    x = undefined
+    y = undefined
+
 })
 
 function drawCircle (x, y) {
