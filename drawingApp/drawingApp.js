0491c5493e321a2a32c3994a3639e8b341d80b00
Meredith Jones <mj19003@gmail.com>
2021-06-18 17:14:34 -0400

    Added event listener to canvas on mousedown

--- drawingApp/drawingApp.js
+++ drawingApp/drawingApp.js
@@ -4,6 +4,17 @@ const ctx = canvas.getContext('2d');
 
 let size = 20
 let color = 'black'
+let x
+let y
+
+canvas.addEventListener('mousedown', (e) => {
+    isPressed = true 
+
+    x = e.offsetX
+    y = e.offsetY
+
+    console.log(isPressed, x, y)
+})
 
 function drawCircle (x, y) {
     ctx.beginPath();
@@ -21,5 +32,6 @@ function drawLine(x1, y1, x2, y2) {
     ctx.stroke()
 }
 
-drawCircle(200, 400)
-drawLine(300, 300, 300, 500)
+
+// drawCircle(200, 400)
+// drawLine(300, 300, 300, 500)
