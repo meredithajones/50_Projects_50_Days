cb3502efe2114d7249e30ef11a38d97f418826a3
Meredith Jones <mj19003@gmail.com>
2021-06-18 17:04:18 -0400

    Defined variables and wrote drawCircle function

---
+++ drawingApp/drawingApp.js
@@ -0,0 +1,15 @@
+// Boilerplate from https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+let size = 20
+let color = 'black'
+
+function drawCircle (x, y) {
+    ctx.beginPath();
+    ctx.arc(x, y, size, 0, Math.PI * 2)
+    ctx.fillStyle = color
+    ctx.fill()
+}
+
+drawCircle(200, 400)
