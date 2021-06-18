0fa066454d6528dc6acbf13cbeb3c8294f26ef6c
Meredith Jones <mj19003@gmail.com>
2021-06-17 22:57:55 -0400

    Added event listeners to empties for drag events

--- dragAndDrop/dragAndDrop.js
+++ dragAndDrop/dragAndDrop.js
@@ -4,6 +4,13 @@ const empties = document.querySelectorAll('.empty');
 fill.addEventListener('dragstart', dragStart)
 fill.addEventListener('dragend', dragEnd)
 
+for(const empty of empties) {
+    empty.addEventListener('dragover', dragOver)
+    empty.addEventListener('dragenter', dragEnter)
+    empty.addEventListener('dragleave', dragLeave)
+    empty.addEventListener('drop', dragDrop)
+}
+
 function dragStart() {
     console.log('drag start')
 }
