6a70ca2afa15f64bfc51b44f837e0bc6a55f1be9
Meredith Jones <mj19003@gmail.com>
2021-06-22 09:27:55 -0400

    Switching over data from HTML to generate dynamically from Javascript

--- contentPlaceholder/contentPlaceholder.js
+++ contentPlaceholder/contentPlaceholder.js
@@ -7,3 +7,9 @@ const date = document.getElementById('date')
 
 const animated_bgs = document.querySelectorAll('.animated-bg')
 const animated_bg_text = document.querySelectorAll('.animated-bg-text')
+
+function getData() {
+    header.innerHTML = '<img src="https://images.unsplash.com/photo-1574802366240-696d5309e51b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1006&q=80" alt="image of jellyfish" />'
+    title.innerHTML = 'Lorem ipsum dolor sit amet consectetur.'
+    excerpt.innerHTML =
+}
