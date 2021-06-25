34081adfaddbc3f5eba6c7b98ea9de9413503088
Meredith Jones <mj19003@gmail.com>
2021-06-25 10:57:56 -0400

    Started javascript to rotate slides, brought in all needed elements from the DOM using querySelector

---
+++ doubleVerticalSlider/dblSlider.js
@@ -0,0 +1,6 @@
+const sliderContainer = document.querySelector('.slider-container')
+const slideright = document.querySelector('.right-slide')
+const slideleft = document.querySelector('left-slide')
+const downBtn = document.querySelector('.down-button')
+const upBtn = document.querySelector('.up-button')
+const slidesLength = slideRight.querySelectorAll('.div').length
