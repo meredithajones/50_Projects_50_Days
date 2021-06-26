98fb839e37c184487c818dc5c50600bbe151acc0
Meredith Jones <mj19003@gmail.com>
2021-06-26 16:30:12 -0400

    Defined variables from the DOM and added an array of messages:

---
+++ toastNotification/toastNotification.js
@@ -0,0 +1,9 @@
+const button = document.getElementById('.button')
+const toasts = document.getElementById('.toasts')
+
+const messages = [
+    'Horray!',
+    'Great Job!',
+    'You Did It!',
+    'Keep Up The Great Work!',
+]
