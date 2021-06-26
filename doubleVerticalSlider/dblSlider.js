52c3109e915fb64d18bdc9a99d2f268bb95a32fe
Meredith Jones <mj19003@gmail.com>
2021-06-26 15:13:36 -0400

    Added if statements to rotate active slides

--- doubleVerticalSlider/dblSlider.js
+++ doubleVerticalSlider/dblSlider.js
@@ -3,8 +3,21 @@ const slideRight = document.querySelector('.right-slide')
 const slideLeft = document.querySelector('.left-slide')
 const downBtn = document.querySelector('.down-button')
 const upBtn = document.querySelector('.up-button')
-const slidesLength = slideRight.querySelectorAll('div').length
+const slideLength = slideRight.querySelectorAll('div').length
 
 let activeSlideIndex = 0;
 
-slideLeft.style.top = `-${(slidesLength- 1) *100}vh`
+slideLeft.style.top = `-${(slideLength- 1) *100}vh`
+
+upBtn.addEventListener('click', () => changeSlide('up'))
+downBtn.addEventListener('click', () => changeSlide('down'))
+
+const changeSlide = (direction) => {
+    const sliderHeight = sliderContainer.clientHeight
+        if(direction === 'up') {
+            activeSlideIndex++
+            if(activeSlideIndex > slideLength -1) {
+                activeSlideIndex = 0
+            }
+        }
+}
