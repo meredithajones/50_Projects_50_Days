7b5a0e658a64415c1350ab308ab1619747243aed
Meredith Jones <mj19003@gmail.com>
2021-06-26 16:46:19 -0400

    Added setTimeout to clear the notifications after 3 seconds

--- toastNotification/toastNotification.js
+++ toastNotification/toastNotification.js
@@ -15,6 +15,12 @@ function createNotification() {
     notify.classList.add('toast')
 
     notify.innerText = getRandomMessage()
+
+    toasts.appendChild(notify)
+
+    setTimeout(() => {
+        notify.remove()
+    }, 3000)
 }
 
 function getRandomMessage(){
