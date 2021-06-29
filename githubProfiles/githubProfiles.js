7d7ad8a7660e4a501e3b2e71742cfd2fc74fd5d8
Meredith Jones <mj19003@gmail.com>
2021-06-29 12:07:15 -0400

    Updated to async await function for getUser

--- githubProfiles/githubProfiles.js
+++ githubProfiles/githubProfiles.js
@@ -1,6 +1,9 @@
 const APIURL = 'https://api.github.com/users/'
 
-function getUser(username) {
-    axios.get(APIURL + username)
-    .then(res => console.log(res))
+getUser('meredithajones')
+
+ async function getUser(username) {
+    const res = await axios(APIURL + username)
+    
+    console.log(res.data)
 }
