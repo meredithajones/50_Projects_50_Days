016cac7e8bbbe6872add5eb7eef7bf2db87780ae
Meredith Jones <mj19003@gmail.com>
2021-06-30 09:22:59 -0400

    Moved html card layout into template literal in javascript

--- githubProfiles/githubProfiles.js
+++ githubProfiles/githubProfiles.js
@@ -1,6 +1,7 @@
 const APIURL = 'https://api.github.com/users/'
 
-const from = document.getElementById('form')
+const main = document.getElementById('main')
+const form = document.getElementById('form')
 const search = document.getElementById('search')
 
 
@@ -12,6 +13,34 @@ const search = document.getElementById('search')
       } catch(err) {
         console.log(err)
     }
+ }
+
+ function createUserCard(user) {
+     const cardHTML = 
+     `  <div class="card">
+     <div>
+         <img src="https://randomuser.me/api/portraits/men/30.jpg" alt="" 
+         class="avatar">
+     </div>
+     <div class="user-info">
+         <h2>J.D.</h2>
+         <p>Lorem ipsum dolor sit amet consectetur 
+             adipisicing elit. Amet, repellendus.</p>
+             <ul>
+                 <li>300 <strong>Followers</strong></li>
+                 <li>100 <strong>Following</strong></li>
+                 <li>17 <strong>Repos</strong></li>
+             </ul>
+
+             <div id="repos">
+                 <a href="#" class="repo">Repo 1</a>
+                 <a href="#" class="repo">Repo 2</a>
+                 <a href="#" class="repo">Repo 3</a>
+             </div>
+     </div>
+</div>`
+
+
  }
 
  form.addEventListener('submit', (e) => {
