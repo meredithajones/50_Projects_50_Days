fbf5b8e3a98073b1ac5950d3bea848d8ab79ca89
Meredith Jones <mj19003@gmail.com>
2021-07-02 10:03:50 -0400

    Setting up event listener for double click

---
+++ doubleHeartClick/dblHeartClick.js
@@ -0,0 +1,10 @@
+const loveMe = document.querySelector('.loveMe')
+const times = document.querySelector('#times')
+
+let clickTime = 0
+
+loveMe.addEventListener('click', (e) => {
+    if(clickTime === 0) {
+        clickTime = new
+    }
+})
