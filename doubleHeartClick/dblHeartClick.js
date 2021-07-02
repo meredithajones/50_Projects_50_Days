de77c609930f308f4c21600f5c56401d7e07e698
Meredith Jones <mj19003@gmail.com>
2021-07-02 10:19:07 -0400

    Double click functionality working with timed click intervals

--- doubleHeartClick/dblHeartClick.js
+++ doubleHeartClick/dblHeartClick.js
@@ -6,6 +6,11 @@ let clickTime = 0
 loveMe.addEventListener('click', (e) => {
     if(clickTime === 0) {
         clickTime = new Date().getTime()
-        console.log(clickTime)
+    } else {
+        if((new Date().getTime() -clickTime) < 800) {
+            console.log(123)
+        } else {
+            clickTime = new Date().getTime()
+        }
     }
 })
