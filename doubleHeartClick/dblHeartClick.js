fc2a6e6af87bc610b82e8d2ce6982b77359062d0
Meredith Jones <mj19003@gmail.com>
2021-07-03 16:45:34 -0400

    Added incrementing times clicked display

--- doubleHeartClick/dblHeartClick.js
+++ doubleHeartClick/dblHeartClick.js
@@ -2,6 +2,7 @@ const loveMe = document.querySelector('.loveMe')
 const times = document.querySelector('#times')
 
 let clickTime = 0
+let timesClicked = 0
 
 loveMe.addEventListener('click', (e) => {
     if(clickTime === 0) {
@@ -22,15 +23,18 @@ const createHeart = (e) => {
     heart.classList.add('fa-heart')
 
     const x = e.clientX
-    const y = e.clientY 
+    const y = e.clientY
 
     const leftOffset = e.target.offsetLeft
-    const topOffset = e.target.offsetTop 
+    const topOffset = e.target.offsetTop
 
     const xInside = x - leftOffset
     const yInside = y - topOffset
 
-   heart.style.top = `${yInside}px`
-   heart.style.left = `${xInside}px`
+    heart.style.top = `${yInside}px`
+    heart.style.left = `${xInside}px`
 
+    loveMe.appendChild(heart)
+
+    times.innerHTML = ++timesClicked
 }
