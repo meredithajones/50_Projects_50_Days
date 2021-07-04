58cbb551b505a3297a701ecbd455c3a13f105916
Meredith Jones <mj19003@gmail.com>
2021-07-04 10:43:05 -0400

    User control of typing speed functioning. Project complete

--- autoTextEffect/autotext.js
+++ autoTextEffect/autotext.js
@@ -1,6 +1,6 @@
 const textEl = document.getElementById('text')
 const speedEl = document.getElementById('speed')
-const text = 'We Love Programing!'
+const text = 'We Love Programming!'
 let idx = 1
 let speed = 300 / speedEl.value 
 
@@ -14,4 +14,9 @@ function writeText() {
     if(idx > text.length) {
         idx = 1
     }
+
+    setTimeout(writeText, speed)
 }
+
+
+speedEl.addEventListener('input', (e) => speed = 300 / e.target.value)
