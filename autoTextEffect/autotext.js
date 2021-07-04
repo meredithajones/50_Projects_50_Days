e6925fc864e1f806f1b9a84f4a97e28e677b4034
Meredith Jones <mj19003@gmail.com>
2021-07-04 10:38:01 -0400

    Added writeText function

--- autoTextEffect/autotext.js
+++ autoTextEffect/autotext.js
@@ -3,3 +3,15 @@ const speedEl = document.getElementById('speed')
 const text = 'We Love Programing!'
 let idx = 1
 let speed = 300 / speedEl.value 
+
+writeText()
+
+function writeText() {
+    textEl.innerText = text.slice(0, idx)
+
+    idx++
+
+    if(idx > text.length) {
+        idx = 1
+    }
+}
