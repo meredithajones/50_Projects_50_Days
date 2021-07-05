7ccccf49573bfde2ab08514fb4f1878ff5dc34fb
Meredith Jones <mj19003@gmail.com>
2021-07-05 16:35:34 -0400

    Fixed error in getRandom funtions

--- passwordGenerator/password.js
+++ passwordGenerator/password.js
@@ -19,9 +19,9 @@ generateEl.addEventListener('click', () => {
     const hasLower = lowercaseEl.checked
     const hasUpper = uppercaseEl.checked
     const hasNumber = numbersEl.checked
-    const hassymbol = symbolsEl.checked
+    const hasSymbol = symbolsEl.checked
 
-    resultEl.innerText = generatePassword(hasLower, hasNumber, hassymbol, hasUpper);
+    resultEl.innerText = generatePassword(hasLower, hasNumber, hasSymbol, hasUpper, length);
     
 })
 
@@ -30,14 +30,22 @@ function generatePassword(lower, upper, number, symbol, length) {
     const typesCount = lower + upper + number + symbol
     const typesArray = [{lower}, {upper}, {number}, {symbol}].
     // using .filter to remove anything in the array with false as a value
-    filter(item => Object.values(item) [0])
+    filter(item => Object.values(item)[0])
 
-    if(typesCount === 0) {
-        return ''
+    if(typesCount <= 1) {
+        return 'Please select at least two character types'
     }
 
-    
-    console.log(typesArray)
+    for(let i = 0; i < length; i += typesCount) {
+        typesArray.forEach(type => {
+            const functionName = Object.keys(type)[0]
+                generatedPassword += randomGen[functionName]()
+            })
+
+        }
+        const finalPassword = generatedPassword.slice(0, length)
+
+        return finalPassword
 }
 
 function getRandomLower() {
@@ -45,7 +53,7 @@ function getRandomLower() {
 }
 
 function getRandomUpper() {
-    return String.fromCharCode(Math.floor(Math.random() * 65) + 97)
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
 }
 
 
@@ -54,5 +62,5 @@ function getRandomNumber() {
 }
 
 function getRandomSymbol() {
-    return String.fromCharCode(Math.floor(Math.random() * 15) + 33)
+    return String.fromCharCode(Math.floor(Math.random() * 7) + 58)
 }
