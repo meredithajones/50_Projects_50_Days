51f0b8cd75af6cf4dfb0f763e00117182084d494
Meredith Jones <mj19003@gmail.com>
2021-07-05 11:54:37 -0400

    Brought in elements from the DOM'

--- passwordGenerator/password.js
+++ passwordGenerator/password.js
@@ -1,3 +1,19 @@
+const resultEl = document.getElementById('result')
+const lengthEl = document.getElementById('length')
+const uppercaseEl = document.getElementById('uppercase')
+const lowercaseEl = document.getElementById('lowercase')
+const numbersEl = document.getElementById('numbers')
+const symbolsEl = document.getElementById('symbols')
+const generateEl = document.getElementById('generate')
+const clipboardEl = document.getElementById('clipboard')
+
+const randomGen = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
+}
+
 function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
 }
@@ -11,5 +27,3 @@ function getRandomNumber() {
 function getRandomSymbol() {
     return String.fromCharCode(Math.floor(Math.random() * 15) + 33)
 }
-
-    console.log(getRandomSymbol());
