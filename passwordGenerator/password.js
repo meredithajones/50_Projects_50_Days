2ec77abf78057c26446f66f9812e93c522f4acca
Meredith Jones <mj19003@gmail.com>
2021-07-05 15:51:37 -0400

    Added event listener function to generateEL

--- passwordGenerator/password.js
+++ passwordGenerator/password.js
@@ -14,10 +14,24 @@ const randomGen = {
     symbol: getRandomSymbol
 }
 
+generateEl.addEventListener('click', () => {
+    const length = +lengthEl.value
+    const hasLower = lowercaseEl.checked
+    const hasUpper = uppercaseEl.checked
+    const hasNumber = numbersEl.checked
+    const hassymbol = symbolsEl.checked
+
+    resultEl.innerText = generatePassword(hasLower, hasNumber, hassymbol, hasUpper);
+    
+})
+
 function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
 }
 
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 65) + 97)
+}
 
 
 function getRandomNumber() {
