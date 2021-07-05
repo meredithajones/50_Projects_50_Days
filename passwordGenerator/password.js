eea775e33c687b685da519d79a022a6e986548f2
Meredith Jones <mj19003@gmail.com>
2021-07-05 11:35:19 -0400

    Added gerRandomUpper and getRandomLower functions for letters

---
+++ passwordGenerator/password.js
@@ -0,0 +1,9 @@
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+}
+
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+}
+
+    console.log(getRandomUpper());
