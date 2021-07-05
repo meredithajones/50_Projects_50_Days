ef59869905e0392d71ef906f86b74369c1b34e8f
Meredith Jones <mj19003@gmail.com>
2021-07-05 11:44:17 -0400

    Added getRandomNumber and getRandomSymbol fucntions

--- passwordGenerator/password.js
+++ passwordGenerator/password.js
@@ -2,8 +2,14 @@ function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
 }
 
-function getRandomUpper() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+
+
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
 }
 
-    console.log(getRandomUpper());
+function getRandomSymbol() {
+    return String.fromCharCode(Math.floor(Math.random() * 15) + 33)
+}
+
+    console.log(getRandomSymbol());
