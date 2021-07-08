6703c508051f36c54347952cad8669fecbef2d11
Meredith Jones <mj19003@gmail.com>
2021-07-08 13:04:34 -0400

    Brought in variables from the DOM

---
+++ checkboxes/checkboxes.js
@@ -0,0 +1,4 @@
+const toggles = document.querySelector('toggle')
+const good = document.querySelector('#good')
+const cheap = document.querySelector('#cheap')
+const fast = document.querySelector('#fast')
