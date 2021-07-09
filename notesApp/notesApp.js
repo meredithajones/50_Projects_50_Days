822649fa140aeabae0556e10c01322a5546cbbe9
Meredith Jones <mj19003@gmail.com>
2021-07-09 14:05:18 -0400

    Inserted html note structure into a template literal in Javascript

--- notesApp/notesApp.js
+++ notesApp/notesApp.js
@@ -4,5 +4,16 @@ addBtn.addEventListener('click', () => addNewNote())
 
 function addNewNote(text = '') {
     const note = document.createElement('div')
-    
+    note.classList.add('note')
+
+    note.innerHTML = `
+    <div class="tools">
+            <button class="edit"><i class="fas fa-edit"></i></button>
+            <button class="delete"><i class="fas fa-trash-alt"></i></button>
+            </div>
+            <div class="main ${text ? "" : "hidden"}"></div>
+            <textarea class="${text ? "hidden" : ""}" style="background-image: url('https://i.pinimg.com/originals/41/1f/37/411f37504d67b1f04d6b47f1f71e3cbf.jpg');"></textarea>
+    `       
+
+        document.body.appendChild(note)
 }
