f904b1350c72a5dbbd1c5332a974db2144c09dd5
Meredith Jones <mj19003@gmail.com>
2021-07-12 17:33:22 -0400

    Setting up runAnimation function

---
+++ animatedCountdown/animatedCountdown.js
@@ -0,0 +1,16 @@
+const nums = document.querySelectorAll('.numbers span')
+const counter = document.querySelector('.counter')
+const finalMessage = document.querySelector('.final')
+const replay = document.querySelector('#replay')
+
+runAnimation()
+
+function runAnimation() {
+    nums.forEach((num, idx) => {
+        const nextToLast = numbers.length -1
+
+        num.addEventListener('animationend', (e) => {
+            
+        })
+    })
+}
