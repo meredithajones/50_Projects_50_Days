636dea1136fba54b2b1d240abe51aaa66c1ea994
Meredith Jones <mj19003@gmail.com>
2021-07-14 16:27:24 -0400

    Added changeImage function

--- imageCarousel/imageCarousel.js
+++ imageCarousel/imageCarousel.js
@@ -2,8 +2,24 @@ const imgs = document.getElementById('imgs')
 const rightButton = document.getElementById('right')
 const leftButton = document.getElementById('left')
 
-const photos = document.querySelectorAll('.img')
+const photo = document.querySelectorAll('#imgs img')
 
 let idx = 0 
 
 let interval = setInterval(run, 2000)
+
+
+function run() {
+    idx++
+    changeImage()
+}
+
+function changeImage() {
+    if(idx > photo.length -1) {
+        idx = 0
+    } else if(idx < 0) {
+        idx = photo.length -1
+    }
+
+    imgs.style.transform = `translateX(#{-idx * 500}px)`
+}
