706ce35e3f814dd1d88fb9083d8fae22e313545d
Meredith Jones <mj19003@gmail.com>
2021-07-18 16:10:19 -0400

    Added event listeners to sqaure for mouseover and mouseout

--- hoverBoard/hoverboard.js
+++ hoverBoard/hoverboard.js
@@ -3,5 +3,11 @@ const colors = [ '#FAF562', '#85EF47', '#8BFFFF', '#EA168E', '#00D1FF', '#FF7A5A
 const SQUARES = 500
 
 for (let i = 0; i < SQUARES; i++) {
-    
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor (square))
+    square.addEventListener('mouseout', () => removeColor (square))
+
+    container.appendChild(square)
 }
