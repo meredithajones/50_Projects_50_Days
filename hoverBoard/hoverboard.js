32ba15cf1238baa6a14937cf170d4dbb12c77c86
Meredith Jones <mj19003@gmail.com>
2021-07-18 15:52:36 -0400

    Defined project variables and added a for loop to Squares

---
+++ hoverBoard/hoverboard.js
@@ -0,0 +1,7 @@
+const container = document.getElementById('container')
+const colors = [ '#FAF562', '#85EF47', '#8BFFFF', '#EA168E', '#00D1FF', '#FF7A5A', '#A100FF', '#FF0000' ]
+const SQUARES = 500
+
+for (let i = 0; i < SQUARES; i++) {
+    
+}
