1a7f736dbcc83faf75e75891c6439ca59706cea3
Meredith Jones <mj19003@gmail.com>
2021-07-25 13:00:28 -0400

    Added function to fetch and console log data from the API successfully

--- pokedex/pokedex.js
+++ pokedex/pokedex.js
@@ -22,3 +22,13 @@ const fetchPokemons = async() => {
         await getPokemon(i)
     }
 }
+
+const getPokemon = async (id) => {
+const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+const res = await fetch(url)
+const data = await res.json()
+console.log(data)
+
+}
+
+fetchPokemons()
