39fcd5adb68ed656da49e9cc31b4b7b729562191
Meredith Jones <mj19003@gmail.com>
2021-07-29 09:42:13 -0400

    Added index to item.classlist:

--- mobileTab/moblieTabNav.js
+++ mobileTab/moblieTabNav.js
@@ -1,17 +1,21 @@
-const contents = document.querySelectorAll('.content');
-const listItems = document.querySelectorAll('nav ul li');
+const contents = document.querySelectorAll('.content')
+const listItems = document.querySelectorAll('nav ul li')
 
 listItems.forEach((item, idx) => {
     item.addEventListener('click', () => {
         hideAllContents()
-        hideAllItems()  
+        hideAllItems()
+
+        item.classList.add('active')
+        contents[idx].classList.add('show')
     })
 })
 
 function hideAllContents() {
-    contents.forEach(content => content.classList.remove('show'));
+    contents.forEach(content => content.classList.remove('show'))
 }
 
+
 function hideAllItems() {
-    contents.forEach(item => item.classList.remove('active'));
+    listItems.forEach(item => item.classList.remove('active'))
 }
