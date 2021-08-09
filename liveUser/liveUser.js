2ca73a1cfe78c4d2be308bff6c3162c3669e197e
Meredith Jones <mj19003@gmail.com>
2021-08-09 17:20:57 -0400

    Destructuring and clearing results

--- liveUser/liveUser.js
+++ liveUser/liveUser.js
@@ -2,6 +2,13 @@ const result = document.getElementById('result');
 const filter = document.getElementById('filter');
 const listItems = [];
 
+getData();
+
 async function getData() {
     const res = await fetch('https://randomuser.me/api?results=50');
+
+    const { results }  = await res.json();
+ 
+    //Clear Results
+    results.innerHTML = ''
 }
