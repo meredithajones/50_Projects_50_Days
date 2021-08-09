44cb8b17ef1edd079e01a07d682682c773a28b91
Meredith Jones <mj19003@gmail.com>
2021-08-09 17:44:53 -0400

    Completed javascript funcitonality, added some more styling

--- liveUser/liveUser.js
+++ liveUser/liveUser.js
@@ -1,41 +1,43 @@
-const result = document.getElementById('result');
-const filter = document.getElementById('filter');
+const result = document.getElementById("result");
+const filter = document.getElementById("filter");
 const listItems = [];
 
 getData();
 
-filter.addEventListener('input', (e) => filterData(e.target.value))
+filter.addEventListener("input", (e) => filterData(e.target.value));
 
 async function getData() {
-    const res = await fetch('https://randomuser.me/api?results=50');
+  const res = await fetch("https://randomuser.me/api?results=50");
 
-    const { results }  = await res.json();
- 
-    //Clear Result
-    result.innerHTML = ''
+  const { results } = await res.json();
 
-    results.forEach(user => {
-        const li = document.createElement('li')
+  //Clear Result
+  result.innerHTML = "";
 
-        listItems.push(li)
+  results.forEach((user) => {
+    const li = document.createElement("li");
 
-        li.innerHTML = `
+    listItems.push(li);
+
+    li.innerHTML = `
             <img src="${user.picture.large}" 
             alt="${user.name.first}">
             <div class="user-info">
             <h4>${user.name.first} ${user.name.last}</h4>
             <p>${user.location.city}, ${user.location.country}</p>
             </div>
-        `
+        `;
 
-        result.appendChild(li)
-    })
+    result.appendChild(li);
+  });
 }
 
-function filterData(searchTerm){
-    listItems.forEach(item => {
-        if(item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
-            item.classList.remove('hide')
-        }
-    })
+function filterData(searchTerm) {
+  listItems.forEach((item) => {
+    if (item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
+      item.classList.remove("hide");
+    } else {
+      item.classList.add("hide");
+    }
+  });
 }
