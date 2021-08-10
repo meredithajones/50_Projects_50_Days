ce604ad43b4d0caa03d2c8966c8956b3ca4faf07
Meredith Jones <mj19003@gmail.com>
2021-08-10 17:45:01 -0400

    Added functionality for targeting by parentNode

--- userFeedback/userFeedback.js
+++ userFeedback/userFeedback.js
@@ -2,6 +2,11 @@ const ratings = document.querySelectorAll('.rating');
 const sendBtn = document.querySelector('#send');
 const panel = document.querySelector('#panel');
 
-panel.addEventListener('click', () => {
-    console.log('click');
+panel.addEventListener('click', (e) => {
+    if(e.target.parentNode.classList.contains('rating')) 
+    {
+        e.target.parentNode.classList.add('active');
+    }
+
 })
+
