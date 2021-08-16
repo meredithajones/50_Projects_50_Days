8365a75ee47cadbfd9932dc0e981bd594daa24e1
Meredith Jones <mj19003@gmail.com>
2021-08-16 17:58:17 -0400

    Added user feedback message functionality, project complete

--- userFeedback/userFeedback.js
+++ userFeedback/userFeedback.js
@@ -1,8 +1,9 @@
 const ratings = document.querySelectorAll('.rating');
+const ratingsContainer = document.querySelector('.ratings-container');
 const sendBtn = document.querySelector('#send');
 const panel = document.querySelector('#panel');
 
-panel.addEventListener('click', (e) => {
+ratingsContainer.addEventListener('click', (e) => {
     if(e.target.parentNode.classList.contains
         ('rating')) {
             removeActive()
@@ -19,8 +20,9 @@ sendBtn.addEventListener('click', (e) => {
         <i class="fas fa-heart"></i>
         <strong>Thank You!</strong>
         <br>
-        <strong>Feedback: </strong>
-    `
+        <strong>Feedback: ${selectedRating}</strong>
+       <p>We'll use your feedback to improve our customer support</p>
+       `
 })
 
 function removeActive() {
