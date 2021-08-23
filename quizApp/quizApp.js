25b5d0b8a111bf71f10ee8314aaf90ba8264d098
Meredith Jones <mj19003@gmail.com>
2021-08-23 16:22:22 -0400

    Added first two questions to quizData in javascript

---
+++ quizApp/quizApp.js
@@ -0,0 +1,17 @@
+const quizData = [
+    {
+        question: "Which language runs in a web browser?",
+        a: "Java",
+        b: "C",
+        c: "Python",
+        d: "JavaScript",
+        correct: "d",
+    },
+    {
+        question: "What does CSS stand for?",
+        a: "Central Style Sheets",
+        b: "Cascading Style Sheets",
+        c: "Cascading Simple Sheets",
+        d: "Cars SUVs Sailboats",
+        correct: "b",
+    },
