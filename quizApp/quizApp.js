fd6f3bf353792b04c3223c3c74561bbb8b99e492
Meredith Jones <mj19003@gmail.com>
2021-08-24 15:23:06 -0400

    Answers dynamically rendering for multiple choice

--- quizApp/quizApp.js
+++ quizApp/quizApp.js
@@ -52,4 +52,8 @@ function loadQuiz() {
     const currentQuizData = quizData[currentQuiz]
 
     questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
 }
