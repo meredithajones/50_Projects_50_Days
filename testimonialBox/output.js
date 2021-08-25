c4d89f75f376bde03b3ebd79f0123bd82c4ec23c
Meredith Jones <mj19003@gmail.com>
2021-08-25 14:21:58 -0400

    Adding html structure

---
+++ testimonialBox/output.js
@@ -0,0 +1,14 @@
+const firstNum = 60;
+const lastNum = 70;
+let outputNum = 0;
+
+// Complete the countUp function below.
+function countUp(start) {
+for( firstNum; firstNum < lastNum; firstNum++ ) {
+    outputNum = outputNum + firstNum;
+}
+    // document.write (outputNum + "then") ;
+    console.log(outputNum);
+}
+
+console.log(outputNum);
