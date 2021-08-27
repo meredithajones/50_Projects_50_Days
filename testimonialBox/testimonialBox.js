b96095d559d3415a114b5a1c69c70406805b5e92
Meredith Jones <mj19003@gmail.com>
2021-08-27 16:58:46 -0400

    Defined all variable from the DOM

---
+++ testimonialBox/testimonialBox.js
@@ -0,0 +1,6 @@
+const testimonialsContainer = document.
+querySelector('.testimonials-container');
+const testimonial = document.querySelector('.testimonial');
+const userImage = document.querySelector('.user-image');
+const userName = document.querySelector('.username');
+const role = document.querySelector('.role');
