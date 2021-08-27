e359970de7e99edafee51e48c56efc6c8f039d0b
Meredith Jones <mj19003@gmail.com>
2021-08-27 17:07:31 -0400

    Finished testimonial arrays

--- testimonialBox/testimonialBox.js
+++ testimonialBox/testimonialBox.js
@@ -5,7 +5,7 @@ const userImage = document.querySelector('.user-image');
 const userName = document.querySelector('.username');
 const role = document.querySelector('.role');
 
-const testimonials = 
+const testimonials = [
 
 {
     name: 'Miyah Myles',
@@ -43,3 +43,20 @@ const testimonials =
     text:
       "I had my concerns that due to a tight deadline this project can't be done. But this guy proved me wrong not only he delivered an outstanding work but he managed to deliver 1 day prior to the deadline. And when I asked for some revisions he made them in MINUTES. I'm looking forward to work with him again and I totally recommend him. Thanks again!",
   },
+  {
+
+  name: 'Sasha Ho',
+  position: 'Accountant',
+  photo:
+    'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?h=350&auto=compress&cs=tinysrgb',
+  text:
+    'This guy is a top notch designer and front end developer. He communicates well, works fast and produces quality work. We have been lucky to work with him!',
+},
+{
+  name: 'Veeti Seppanen',
+  position: 'Director',
+  photo: 'https://randomuser.me/api/portraits/men/97.jpg',
+  text:
+    'This guy is a young and talented IT professional, proactive and responsible, with a strong work ethic. He is very strong in PSD2HTML conversions and HTML/CSS technology. He is a quick learner, eager to learn new technologies. He is focused and has the good dynamics to achieve due dates and outstanding results.',
+},
+]
