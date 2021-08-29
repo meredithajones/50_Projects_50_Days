0ac3e61a0aa7196c41141d83e76daa489856b1ce
Meredith Jones <mj19003@gmail.com>
2021-08-29 16:17:24 -0400

    Wrapped up the javascript, project complete

--- testimonialBox/testimonialBox.js
+++ testimonialBox/testimonialBox.js
@@ -20,21 +20,21 @@ const testimonials = [
     position: 'Software Engineer',
     photo: 'https://randomuser.me/api/portraits/women/44.jpg',
     text:
-      'This guy is an amazing frontend developer that delivered the task exactly how we need it, do your self a favor and hire him, you will not be disappointed by the work delivered. He will go the extra mile to make sure that you are happy with your project. I will surely work again with him!',
+      'She is an amazing frontend developer that delivered the task exactly how we need it, do your self a favor and hire him, you will not be disappointed by the work delivered. He will go the extra mile to make sure that you are happy with your project. I will surely work again with him!',
   },
   {
     name: 'Iida Niskanen',
     position: 'Data Entry',
     photo: 'https://randomuser.me/api/portraits/women/68.jpg',
     text:
-      "This guy is a hard worker. Communication was also very good with him and he was very responsive all the time, something not easy to find in many freelancers. We'll definitely repeat with him.",
+      "This lady is a hard worker. Communication was also very good with him and he was very responsive all the time, something not easy to find in many freelancers. We'll definitely repeat with him.",
   },
   {
     name: 'Renee Sims',
     position: 'Receptionist',
     photo: 'https://randomuser.me/api/portraits/women/65.jpg',
     text:
-      "This guy does everything he can to get the job done and done right. This is the second time I've hired him, and I'll hire him again in the future.",
+      "They do everything they can to get the job done and done right. This is the second time I've hired him, and I'll hire him again in the future.",
   },
   {
     name: 'Jonathan Nunfiez',
@@ -50,14 +50,14 @@ const testimonials = [
   photo:
     'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?h=350&auto=compress&cs=tinysrgb',
   text:
-    'This guy is a top notch designer and front end developer. He communicates well, works fast and produces quality work. We have been lucky to work with him!',
+    'They are a top notch designer and front end developer. He communicates well, works fast and produces quality work. We have been lucky to work with him!',
 },
 {
   name: 'Veeti Seppanen',
   position: 'Director',
   photo: 'https://randomuser.me/api/portraits/men/97.jpg',
   text:
-    'This guy is a young and talented IT professional, proactive and responsible, with a strong work ethic. He is very strong in PSD2HTML conversions and HTML/CSS technology. He is a quick learner, eager to learn new technologies. He is focused and has the good dynamics to achieve due dates and outstanding results.',
+    'She is a young and talented IT professional, proactive and responsible, with a strong work ethic. He is very strong in PSD2HTML conversions and HTML/CSS technology. He is a quick learner, eager to learn new technologies. He is focused and has the good dynamics to achieve due dates and outstanding results.',
 },
 ]
 
@@ -72,6 +72,10 @@ function updateTestimonial() {
    role.innerHTML = position 
 
    idx++
+
+   if(idx > testimonials.length -1) {
+        idx = 0
+   }
 }
 
-updateTestimonial();
+setInterval(updateTestimonial, 10000);
