f10acbc6862017d2c924a76ee88c553ee6f39966
Meredith Jones <mj19003@gmail.com>
2021-08-31 18:29:22 -0400

    Created a for loop to add images to the dom from unsplashURL

--- randomImage/randomImage.js
+++ randomImage/randomImage.js
@@ -2,3 +2,7 @@ const container = document.querySelector('.container');
 const unSplashURL = 'https://source.unspash.com/random/'
 const rows = 10
 
+for(let i = 0; 1 < rows * 3; i++) {
+    const img = document.createElement('img')
+    img.src = `${unSplashURL}`
+}
