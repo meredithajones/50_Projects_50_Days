032fd87f93fca9e329e8259faf516769007be056
Meredith Jones <mj19003@gmail.com>
2021-09-09 11:59:15 -0400

    Working to identlify bug in javascript

--- randomImage/randomImage.js
+++ randomImage/randomImage.js
@@ -1,18 +1,3 @@
 const container = document.querySelector('.container');
 const unSplashURL = 'https://source.unspash.com/random/'
 const rows = 10
-
-for(let i = 0; 1 < rows * 3; i++) {
-    const img = document.createElement('img')
-    img.src = `${unSplashURL}`
-}
-
-console.log(getRandomNr);
-
-function getRandomSize() {
-    return `${getRandomNr()}x${getRandomNr()}`
-}
-
-function getRandomNr() {
-    return Math.floor(Math.random() * 10) + 300
-}
