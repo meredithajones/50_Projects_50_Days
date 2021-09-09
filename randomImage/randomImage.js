c319f0f2b7fa3c6acd9f9b5e94ec8152290089e2
Meredith Jones <mj19003@gmail.com>
2021-09-09 12:06:03 -0400

    fixed javascript bug

--- randomImage/randomImage.js
+++ randomImage/randomImage.js
@@ -1,4 +1,17 @@
 const container = document.querySelector('.container');
-const unSplashURL = 'https://source.unspash.com/random/'
-const rows = 10
+const unSplashURL = 'https://source.unsplash.com/random/'
+const rows = 5
 
+for(let i = 0; i < rows * 3; i++) {
+    const img = document.createElement('img')
+    img.src = `${unSplashURL}${getRandomSize()}`
+    container.appendChild(img)
+}
+
+function getRandomSize() {
+    return `${getRandomNr()}x${getRandomNr()}`
+}
+
+function getRandomNr() {
+    return Math.floor(Math.random() * 10) + 300
+}
