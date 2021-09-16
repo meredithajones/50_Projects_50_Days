0a3eeaf91875a7f5851a1ac16404357a2f54f7f3
Meredith Jones <mj19003@gmail.com>
2021-09-16 15:06:56 -0400

    Defined variables and added an event listener on submit

---
+++ toDoList/app.js
@@ -0,0 +1,7 @@
+const form = document.getElementById('form')
+const input = document.getElementById('input')   
+const todoUL = document.getElementById('todos')
+
+form.addEventListener('submit', (e)=> {
+    e.preventDefault()
+})
