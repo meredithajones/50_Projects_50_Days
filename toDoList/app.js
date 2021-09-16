d1fdef300aacef4eb73f19ff96ddbf192abcf3bd
Meredith Jones <mj19003@gmail.com>
2021-09-16 15:11:45 -0400

    wrote addToDo function, console logging to do text successfully

--- toDoList/app.js
+++ toDoList/app.js
@@ -3,5 +3,17 @@ const input = document.getElementById('input')
 const todoUL = document.getElementById('todos')
 
 form.addEventListener('submit', (e)=> {
-    e.preventDefault()
+    e.preventDefault();
+
+    addTodo()
 })
+
+function addTodo(todo) {
+    let todoText = input.value 
+
+    if(todo) {
+        todoText = todo.text
+    }
+
+    console.log(todoText)
+}
