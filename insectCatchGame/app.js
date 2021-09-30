f56e943ffa8a3832b36e7db9a5c0a4344439f228
Meredith Jones <mj19003@gmail.com>
2021-09-30 09:31:52 -0400

    Added javascript comments

--- insectCatchGame/app.js
+++ insectCatchGame/app.js
@@ -11,13 +11,14 @@ let selected_insect = {};
 
 start_btn.addEventListener("click", () => screens[0].classList.add("up"));
 
+// Selecting insect on click, assigning it an image and adding a class of 'up' to move screen up
 choose_insect_btns.forEach((btn) => {
   btn.addEventListener("click", () => {
     const img = btn.querySelector("img");
     const src = img.getAttribute("src");
     const alt = img.getAttribute("alt");
     selected_insect = { src, alt };
-    screens[1].classlist.add('up');
+    screens[1].classlist.add("up");
     setTimeout(createInsect, 1000);
     startGame();
   });
@@ -25,12 +26,19 @@ choose_insect_btns.forEach((btn) => {
 
 // Inserting insect into the DOM
 function createInsect() {
-    const insect = document.createElement('div')
-    insect.classList.add('insect')
-    const { x, y } = getRandomLocation()
+  const insect = document.createElement("div");
+  insect.classList.add("insect");
+  const { x, y } = getRandomLocation();
+  insect.style.top = `${y}px`
+  insect.style.left = `${x}px`
+
 }
 
+// Creating random insect location based on window height & width
 function getRandomLocation() {
-    const width = window.innerWidth
-    const height = window.innerHeight
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  const x = Math.random() * (width - 200) + 100;
+  const y = Math.random() * (height - 200) + 100;
+  return { x, y}
 }
