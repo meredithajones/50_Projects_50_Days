2d054f13de2acf4be97ea2731e42d1eff9615740
Meredith Jones <mj19003@gmail.com>
2021-09-30 09:13:52 -0400

    Added an onclick event liste3ner to start_btn to add class of up

--- insectCatchGame/app.js
+++ insectCatchGame/app.js
@@ -1,7 +1,13 @@
 const screens = document.querySelectorAll('.screen');
 const choose_insect_btns = document.querySelectorAll('.choose-insect-btn');
+const game_container = document.getElementById('game-container');
 const start_btn = document.getElementById('start-btn');
 const timeEL = document.getElementById('time');
 const scoreEL = document.getElementById('score');
 const message = document.getElementById('message');
-const game_container = document.getElementById('game-container');
+let seconds = 0;
+let score = 0;
+let selected_insect = {};
+
+start_btn.addEventListener('click', () => screens 
+[0].classList.add('up'));
