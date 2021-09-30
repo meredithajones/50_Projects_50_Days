e91269a2aaf17888d1ba7499e41bb76c44260e08
Meredith Jones <mj19003@gmail.com>
2021-09-30 09:09:42 -0400

    Brought elements from the DOM into javascript

--- insectCatchGame/app.js
+++ insectCatchGame/app.js
@@ -1,3 +1,7 @@
 const screens = document.querySelectorAll('.screen');
-
-screens[0].classList.add('up')
+const choose_insect_btns = document.querySelectorAll('.choose-insect-btn');
+const start_btn = document.getElementById('start-btn');
+const timeEL = document.getElementById('time');
+const scoreEL = document.getElementById('score');
+const message = document.getElementById('message');
+const game_container = document.getElementById('game-container');
