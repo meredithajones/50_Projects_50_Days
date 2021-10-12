d87861eddf5e4952d406daa29d5882dd0df5e429
Meredith Jones <mj19003@gmail.com>
2021-10-12 17:44:48 -0400

    Fixed bug in javascript

--- 3DBackgroundSquares/app.js
+++ 3DBackgroundSquares/app.js
@@ -2,16 +2,14 @@ const boxesContainer = document.getElementById('boxes');
 const btn = document.getElementById('btn');
 
 function createBoxes() {
-    for(let i =0; 1 < 4; i++) {
-        for (let j = 0; j <4; j++) {
-            // creating boxes with loops
-            const box = document.createElement('div');
-            box.classList.add('box');
-            // Using a template literal to position boxes
-            box.style.backgroundPosition = `${-j * 125px} ${-i * 125px}`
-            boxesContainer.appendChild(box)
-        }
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        const box = document.createElement('div')
+        box.classList.add('box')
+        box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
+        boxesContainer.appendChild(box)
+      }
     }
-}
-
-createBoxes()
+  }
+  
+  createBoxes()
