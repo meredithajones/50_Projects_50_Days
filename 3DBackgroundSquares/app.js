75f97f2955f86dea0c6c64e288bc5ee07cba66e5
Meredith Jones <mj19003@gmail.com>
2021-10-12 17:30:22 -0400

    Added 2 for loops to createBoxes function

--- 3DBackgroundSquares/app.js
+++ 3DBackgroundSquares/app.js
@@ -2,7 +2,11 @@ const boxesContainer = document.getElementById('boxes');
 const btn = document.getElementById('btn');
 
 function createBoxes() {
-
+    for(let i =0; 1 < 4; i++) {
+        for (let j = 0; j <4; j++) {
+            
+        }
+    }
 }
 
 createBoxes();
