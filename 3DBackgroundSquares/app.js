fd3575c937c9f9232a7e5a08c4804cd5861c6ad4
Meredith Jones <mj19003@gmail.com>
2021-10-12 17:39:06 -0400

    Added template literal to position boxes

--- 3DBackgroundSquares/app.js
+++ 3DBackgroundSquares/app.js
@@ -4,7 +4,12 @@ const btn = document.getElementById('btn');
 function createBoxes() {
     for(let i =0; 1 < 4; i++) {
         for (let j = 0; j <4; j++) {
-            
+            // creating boxes with loops
+            const box = document.createElement('div');
+            box.classList.add('box');
+            // Using a template literal to position boxes
+            box.style.backgroundPosition = `${-j * 125} ${-i * 125}`
+            boxesContainer.appendChild(box)
         }
     }
 }
